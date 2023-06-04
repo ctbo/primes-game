2868eab678b163cfce9d976ac23411a0c03a91e4
Harald Bögeholz <harald@boegeholz.org>
2023-06-04 15:49:45 +1000

    switching to Google Noto Emoji images.

--- resources/makeeps.py
+++ resources/makeeps.py
@@ -28,5 +28,36 @@ symbols = {
     97: "pile-of-poo"
 }
 
-for prime, filename in symbols.items():
-    subprocess.run(f"convert {filename}.png -background none -flatten EPS3:{prime}.eps", shell=True)
+symbols_unicode = {
+    2: "1F34E",
+    3: "1FAD0",
+    5: "1F34C", # banana; tangerine: "1F34A",
+    7: "1F951",
+    11: "1F424",
+    13: "1F416",
+    17: "1F411",
+    19: "1F40E",
+    23: "1F419",
+    29: "1F980",
+    31: "1F42C", # dolphin. spouting-whale: "1F433",
+    37: "1F41F", # tropical-fish: "1F420",
+    41: "1F40A", # crocodile. "1F438", # frog. lizard: "1F98E",
+    43: "1F422",
+    47: "1F40D",
+    53: "1F418",
+    59: "1F98F", # rhino. zebra: "1F993",
+    61: "1F42A",
+    67: "1F992",
+    71: "1FAB3",
+    73: "1FAB2",
+    79: "1F997",
+    83: "1F996",
+    89: "1F995",
+    97: "1F4A9"
+}
+
+# for prime, filename in symbols.items():
+#     subprocess.run(f"convert {filename}.png -background none -flatten EPS3:{prime}.eps", shell=True)
+
+for prime, filename in symbols_unicode.items():
+    subprocess.run(f"cairosvg /Users/hwb/Desktop/noto-emoji/svg/emoji_u{filename.lower()}.svg -s 0.5 -o {prime}.eps", shell=True)
