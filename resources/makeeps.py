8f1e60fb4c84c1310688e517df2fafd5c7d3f907
Harald Bögeholz <harald@boegeholz.org>
2023-06-20 17:33:23 +1000

    added white border around all emoji.

--- resources/makeeps.py
+++ resources/makeeps.py
@@ -2,31 +2,31 @@ import subprocess
 
 
 symbols_unicode = {
-    2: "1F34E",
-    3: "1FAD0",
+    2: "1F34E-bo",
+    3: "1FAD0-bo",
     5: "1F34C-bo", # banana; tangerine: "1F34A",
-    7: "1F951",
+    7: "1F951-bo",
     11: "1F424-bo", # chick
-    13: "1F416",
+    13: "1F416-bo",
     17: "1F411-bo", # sheep
-    19: "1F40E",
-    23: "1F419",
-    29: "1F980",
-    31: "1F42C", # dolphin. spouting-whale: "1F433",
-    37: "1F41F", # tropical-fish: "1F420",
-    41: "1F40A", # crocodile. "1F438", # frog. lizard: "1F98E",
-    43: "1F422",
-    47: "1F40D",
-    53: "1F418",
-    59: "1F98F", # rhino. zebra: "1F993",
-    61: "1F42A",
-    67: "1F992",
-    71: "1F41E", # lady-beetle. cockroach: "1FAB3",
+    19: "1F40E-bo",
+    23: "1F419-bo",
+    29: "1F980-bo",
+    31: "1F42C-bo", # dolphin. spouting-whale: "1F433",
+    37: "1F41F-bo", # tropical-fish: "1F420",
+    41: "1F40A-bo", # crocodile. "1F438", # frog. lizard: "1F98E",
+    43: "1F422-bo",
+    47: "1F40D-bo",
+    53: "1F418-bo",
+    59: "1F98F-bo", # rhino. zebra: "1F993",
+    61: "1F42A-bo",
+    67: "1F992-bo",
+    71: "1F41E-bo", # lady-beetle. cockroach: "1FAB3",
     73: "1F41D-bo", # bee. beetle: "1FAB2",
-    79: "1F98B", # butterfly. cricket: "1F997",
-    83: "1F996",
-    89: "1F995",
-    97: "1F4A9"
+    79: "1F98B-bo", # butterfly. cricket: "1F997",
+    83: "1F996-bo",
+    89: "1F995-bo",
+    97: "1F4A9-bo"
 }
 
 for prime, filename in symbols_unicode.items():
